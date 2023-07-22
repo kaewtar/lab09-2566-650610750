5b84caefba179a1a71ac45e4bf24afc97aeee9de
kaewtar <kaewtarcmu@gmail.com>
2023-07-22 10:46:27 +0700

    completed

--- src/app/page.js
+++ src/app/page.js
@@ -62,7 +62,7 @@ export default function Home() {
       </div>
 
       {/* //footer section */}
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="12345678" />
+      <Footer year="2023" fullName="Kaewtar Lungta" studentId="650610750" />
     </div>
   );
 }
