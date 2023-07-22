bbcf3b11b95929e6649a08eef3423a101afde0f5
kaewtar <kaewtarcmu@gmail.com>
2023-07-22 10:42:56 +0700

    completed

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -18,13 +18,6 @@ export const Task = ({
 
   return (
     <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-      {/*
-      HINTS: if task is completed, below "span" will show like this 
-        <span className="text-decoration-line-through">{title}</span>
-        But if task is not completed : 
-        <span>{title}</span>
-      */}
-
       <span className={result}>{title}</span>
       <button className="btn btn-success" onClick={doneBtnOnClick}>
         Done
